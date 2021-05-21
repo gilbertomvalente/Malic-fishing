94083d646c842362cb51641e18c7d8bc4984cfd6
gilbertomvalente <gilbertomvalente@gmail.com>
2021-05-21 16:20:46 -0300

    Projeto finalizado, com menus responsivos, em 21 de maio de 2021 parte 2

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
-
+/*
 const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -16,3 +16,4 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+*/
